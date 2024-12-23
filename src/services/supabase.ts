6d9183666c181b73cc464606a84e49b7e9193f4d
Xprriacst <expertiaen5min@gmail.com>
2024-12-23 22:44:25 +0100

    Update supabase.ts

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from '../types/supabase';
+import { ENV } from '../config/env.config';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+if (!ENV.SUPABASE.URL || !ENV.SUPABASE.ANON_KEY) {
+  throw new Error('Missing Supabase configuration');
+}
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient<Database>(
+  ENV.SUPABASE.URL,
+  ENV.SUPABASE.ANON_KEY
+);
