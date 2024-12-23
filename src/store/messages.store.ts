35a018647df0f0c0964796e48ed2f5ef5e05736b
Xprriacst <expertiaen5min@gmail.com>
2024-12-23 18:26:16 +0100

    Create messages.store.ts

---
+++ src/store/messages.store.ts
@@ -0,0 +1,17 @@
+import { create } from 'zustand';
+import { Message } from '../types/message.types';
+
+interface MessagesState {
+  messages: Message[];
+  addMessage: (message: Message) => void;
+  setMessages: (messages: Message[]) => void;
+}
+
+export const useMessagesStore = create<MessagesState>((set) => ({
+  messages: [],
+  addMessage: (message) => 
+    set((state) => ({
+      messages: [...state.messages, message]
+    })),
+  setMessages: (messages) => set({ messages })
+}));
